0704ee0b82ffe3059894f1719ff288af9784e9d8
Alizain7866 <am07565@st.habib.edu.pk>
2025-06-17 15:11:03 +0500

    Test Final Commit with SSH Key setup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,8 @@ import org.example.classes.Calculator
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+//The main folder which should run when we run the application
 fun main() {
 
     val calc = Calculator()
